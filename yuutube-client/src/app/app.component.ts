c1f2c3b5d149aa2e032e33c168c7eddf9572b378
VlaDrozd <vladrozd2000@gmail.com>
2020-02-27 17:21:37 +0300

    refactor: delete all functionality from app-component

--- yuutube-client/src/app/app.component.ts
+++ yuutube-client/src/app/app.component.ts
@@ -7,15 +7,5 @@ import { SearchResponse } from './shared-module/models/search-response.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public sortType: string = 'date';
-  public searchResponse: SearchResponse;
 
-  public getSortType(type: string): void {
-    this.sortType = type;
-    console.log(type);
-  }
-
-  public getResponse(response: SearchResponse): void {
-    this.searchResponse = response;
-  }
 }
